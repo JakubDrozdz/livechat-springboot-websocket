043d2228478afee0ed04aabb04a1852d7485c176
Jakub Drozdz <jakubekso@outlook.com>
2023-09-18 19:27:34 +0200

    Project init

---
+++ src/main/java/pl/jakubdrozdz/livechatspringwebsocket/LivechatSpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubdrozdz.livechatspringwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivechatSpringWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivechatSpringWebsocketApplication.class, args);
+    }
+
+}
